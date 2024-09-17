e5bef1b278cb583d9891410e15beb6bc10e7ecdd
netanel <nati09986@gmail.com>
2024-09-17 14:09:39 +0300

    improve the logger

--- src/main/java/com/example/flights_service/controllers/FlightController.java
+++ src/main/java/com/example/flights_service/controllers/FlightController.java
@@ -2,8 +2,7 @@ package com.example.flights_service.controllers;
 
 import com.example.flights_service.models.Flight;
 import com.example.flights_service.services.FlightService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,15 +13,15 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/flights")
+@Slf4j
 public class FlightController {
-    private static final Logger logger = LoggerFactory.getLogger(FlightController.class);
 
     @Autowired
     private FlightService flightService;
 
     @GetMapping("/{id}")
     public ResponseEntity<Flight> getFlight(@PathVariable String id) {
-        logger.info("Fetching flight details for ID: {}", id);
+        log.info("Fetching flight details for ID: {}", id);
         try {
             Flight flight = flightService.getFlightById(id);
             if (flight != null) {
@@ -31,19 +30,19 @@ public class FlightController {
                 return ResponseEntity.notFound().build();
             }
         } catch (IOException | ParseException e) {
-            logger.error("Error retrieving flight with ID: {}", id, e);
+            log.error("Error retrieving flight with ID: {}", id, e);
             return ResponseEntity.status(500).body(null);
         }
     }
 
     @PostMapping("/update")
     public ResponseEntity<String> updateFlights(@RequestBody List<Flight> flights) {
-        logger.info("Updating flights with {} entries", flights.size());
+        log.info("Updating flights with {} entries", flights.size());
         try {
             flightService.updateFlights(flights);
             return ResponseEntity.ok("CSV file updated successfully");
         } catch (IOException | ParseException e) {
-            logger.error("Error updating the CSV file", e);
+            log.error("Error updating the CSV file", e);
             return ResponseEntity.status(500).body("Error updating the CSV file");
         }
     }
